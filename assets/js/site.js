86bf11b1f71d8cbc95f54227c3f038b9075f1013
Rebecca <rubyDoomsday@users.noreply.github.com>
2024-03-18 18:13:39 -0500

    migrate assets

---
+++ assets/js/site.js
@@ -0,0 +1,13 @@
+/**
+ * Menu
+ */
+ $("a.menu-icon").on("click", function(event) {
+   var w = $(".menu");
+
+   w.css({
+     display: w.css("display") === "none"
+      ? "block"
+      : "none"
+   });
+ });
+
